58b4da67a42b923e7f02825f4a6abccefa2d5474
Roman Laitarenko <sfm.kmr@gmail.com>
2012-12-16 17:50:07 +0200

    Initial Commit

---
+++ InteractiveTour/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  InteractiveTour
+//
+//  Created by Roman Laitarenko on 12/16/12.
+//  Copyright (c) 2012 Roman Laitarenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
