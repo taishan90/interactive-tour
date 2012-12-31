3b795f0b08ffcbdd15ca1e3aeec88feb721712ea
Roman Laitarenko <sfm.kmr@gmail.com>
2012-12-31 20:00:56 +0200

    dummy

---
+++ InteractiveTour/MyRunLoop.m
@@ -0,0 +1,13 @@
+//
+//  MyRunLoop.m
+//  InteractiveTour
+//
+//  Created by Roman Laitarenko on 12/31/12.
+//  Copyright (c) 2012 Roman Laitarenko. All rights reserved.
+//
+
+#import "MyRunLoop.h"
+
+@implementation MyRunLoop
+
+@end
