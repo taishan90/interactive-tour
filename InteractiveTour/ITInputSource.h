3a1d3b8766aef7afe83a72951ffae2b2a444418d
Roman Laitarenko <rol@localhost.(none)>
2013-01-16 01:15:48 +0200

    Added container for run loop

---
+++ InteractiveTour/ITInputSource.h
@@ -0,0 +1,21 @@
+//
+//  ITInputSource.h
+//  InteractiveTour
+//
+//  Created by Roman Laitarenko on 1/15/13.
+//  Copyright (c) 2013 Roman Laitarenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^MyBlock)(void);
+
+@interface ITInputSource : NSObject
+
+@property (atomic, readonly, retain) id         object;
+@property (atomic, readonly, assign) SEL        selector;
+@property (atomic, readonly, copy) MyBlock    block;
+
+- (id)initWithObject:(id)anObject selector:(SEL)aSelector block:(MyBlock)aBlock;
+
+@end
